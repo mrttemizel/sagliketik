8f9964a8e2cee1905b412e384a4a30c244b3d193
mrttemizel <murat.temizel@antalya.edu.tr>
2023-09-14 22:22:16 +0300

    super admin change

--- resources/views/backend/components/_partials/_topbar.blade.php
+++ resources/views/backend/components/_partials/_topbar.blade.php
@@ -41,12 +41,12 @@
                             <span class="text-start ms-xl-2">
                                 <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ Auth::user()->name }}</span>
                                 <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">
-                                    @if (Auth::user()->status == 2 )
+                                    @if (Auth::user()->status == 1 )
                                         Super Admin
-                                    @elseif(Auth::user()->status == 1 )
+                                    @elseif(Auth::user()->status == 2 )
                                         Admin
                                     @else
-                                        Editor
+                                        User
                                     @endif
                                 </span>
                             </span>
